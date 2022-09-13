6c4623b9a82512d1558b29d9a911b40f0cb7007a
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-09-14 02:12:19 +0530

    reverted last commit due to buggy scroll features

---
+++ lib/view/products/sales_history.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:migo/layout/layout.dart';
+
+class SalesHistory extends StatelessWidget {
+  const SalesHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppLayout(
+        activeTab: 1,
+        content: Container(),
+        pageName: "Sales History",
+      ),
+    );
+  }
+}
