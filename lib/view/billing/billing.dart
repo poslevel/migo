77b8939a03fecb65538c6fa8c1b07cf2559d06b9
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-09-15 14:49:45 +0530

    initialized billing page

--- lib/view/billing/billing.dart
+++ lib/view/billing/billing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:migo/layout/layout.dart';
+import 'package:migo/view/responsive.dart';
 
 class Billing extends StatelessWidget {
   const Billing({super.key});
@@ -8,8 +9,29 @@ class Billing extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppLayout(
       activeTab: 0,
-      content: Row(
-        children: const [],
+      content: Column(
+        children: [
+          Column(
+            children: [
+              Row(),
+              const Divider(),
+            ],
+          ),
+          SizedBox(
+            width: !Responsive.isMobile(context)
+                ? MediaQuery.of(context).size.width - 110
+                : null,
+            height: !Responsive.isMobile(context)
+                ? MediaQuery.of(context).size.height - 150
+                : MediaQuery.of(context).size.height - 220,
+            child: Row(
+              children: [
+                Expanded(child: Container()),
+                Expanded(child: Container()),
+              ],
+            ),
+          )
+        ],
       ),
       pageName: "Billing",
     );
