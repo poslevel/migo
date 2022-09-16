f92f3a7056b94ad7810a25a7a3e5bac27aebe0ee
manas1820 <manasgupta1820@gmail.com>
2022-09-16 12:58:16 +0530

    feat : add a class for ```cache``` management

---
+++ lib/models/cache.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:get_storage/get_storage.dart';
+
+mixin CacheManager {
+  Future<bool> saveToken(String? token) async {
+    final box = GetStorage();
+    await box.write(CacheManagerKey.TOKEN.toString(), token);
+    return true;
+  }
+
+  String? getToken() {
+    final box = GetStorage();
+    return box.read(CacheManagerKey.TOKEN.toString());
+  }
+
+  Future<void> removeToken() async {
+    final box = GetStorage();
+    await box.remove(CacheManagerKey.TOKEN.toString());
+  }
+}
+
+enum CacheManagerKey { TOKEN }
