4de8350dc862f728c6fe84f4d14d12c84f22c919
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-09-16 17:49:21 +0530

    mofularized customer info page

--- lib/view/billing/billing.dart
+++ lib/view/billing/billing.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:migo/controller/customer_details_controller.dart';
-import 'package:migo/controller/product_controller.dart';
 import 'package:migo/layout/layout.dart';
 import 'package:migo/view/billing/add_products_page.dart';
+import 'package:migo/view/billing/customer_info_page.dart';
 import 'package:migo/view/responsive.dart';
-import 'package:migo/widgets/billing_page_divider.dart';
-import 'package:migo/widgets/buttons.dart';
-import 'package:migo/widgets/product_link_opener.dart';
 
 class Billing extends StatefulWidget {
   const Billing({super.key});
@@ -110,7 +105,7 @@ class _BillingState extends State<Billing> with SingleTickerProviderStateMixin {
               AddProductsPage(
                 tabController: _tabController,
               ),
-              _CustomerInfoPage(
+              CustomerInfoPage(
                 nameController: nameController,
                 emailController: emailController,
                 phonenumberController: phonenumberController,
@@ -122,133 +117,6 @@ class _BillingState extends State<Billing> with SingleTickerProviderStateMixin {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class _CustomerInfoPage extends StatelessWidget {
-  const _CustomerInfoPage({
-    Key? key,
-    required this.nameController,
-    required this.emailController,
-    required this.phonenumberController,
-    required this.addressController,
-    required this.formkey,
-    required this.tabController,
-  }) : super(key: key);
-
-  final TextEditingController nameController;
-  final TextEditingController emailController;
-  final TextEditingController phonenumberController;
-  final TextEditingController addressController;
-  final GlobalKey<FormState> formkey;
-  final TabController tabController;
-
-  @override
-  Widget build(BuildContext context) {
-    final CustomerDetailsController _customerDetailsController =
-        Get.put(CustomerDetailsController());
-    return Form(
-      key: formkey,
-      child: Column(
-        children: [
-          const BillingPageDivider(),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.user_octagon),
-                          label: Text("Customer Name"),
-                          hintText: "Jhon Doe"),
-                      controller: nameController,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Name';
-                        }
-                        return null;
-                      }),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Iconsax.sms),
-                        label: Text("E-mail"),
-                        hintText: "jhon.doe@example.com",
-                      ),
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Email';
-                        }
-                        if (!value.isEmail) {
-                          return 'Please enter valid Email';
-                        }
-                        return null;
-                      }),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.call),
-                          label: Text("Phone Number"),
-                          hintText: "9553052451"),
-                      controller: phonenumberController,
-                      keyboardType: TextInputType.phone,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Phone number';
-                        }
-                        if (!value.isPhoneNumber) {
-                          return 'Please enter a valid Phone number';
-                        }
-                        return null;
-                      }),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      maxLines: 3,
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.location),
-                          label: Text("Address"),
-                          hintText: "Where does your customer live"),
-                      controller: addressController,
-                    ),
-                  ),
-                  PrimaryButton(
-                    onPressed: () {
-                      // Validate returns true if the form is valid, or false otherwise.
-                      if (formkey.currentState!.validate()) {
-                        // ... Navigate To next page
-                        _customerDetailsController.name.value =
-                            nameController.text;
-                        _customerDetailsController.email.value =
-                            emailController.text;
-                        _customerDetailsController.phone.value =
-                            phonenumberController.text;
-                        _customerDetailsController.address.value =
-                            addressController.text;
-                        tabController.animateTo(2);
-                      }
-                    },
-                    buttonTitle: "Continue",
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/view/billing/customer_info_page.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:migo/controller/customer_details_controller.dart';
+import 'package:migo/widgets/billing_page_divider.dart';
+import 'package:migo/widgets/buttons.dart';
+
+class CustomerInfoPage extends StatelessWidget {
+  const CustomerInfoPage({
+    Key? key,
+    required this.nameController,
+    required this.emailController,
+    required this.phonenumberController,
+    required this.addressController,
+    required this.formkey,
+    required this.tabController,
+  }) : super(key: key);
+
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController phonenumberController;
+  final TextEditingController addressController;
+  final GlobalKey<FormState> formkey;
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    final CustomerDetailsController _customerDetailsController =
+        Get.put(CustomerDetailsController());
+    return Form(
+      key: formkey,
+      child: Column(
+        children: [
+          const BillingPageDivider(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          icon: Icon(Iconsax.user_octagon),
+                          label: Text("Customer Name"),
+                          hintText: "Jhon Doe"),
+                      controller: nameController,
+                      validator: ((value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Name';
+                        }
+                        return null;
+                      }),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Iconsax.sms),
+                        label: Text("E-mail"),
+                        hintText: "jhon.doe@example.com",
+                      ),
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: ((value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Email';
+                        }
+                        if (!value.isEmail) {
+                          return 'Please enter valid Email';
+                        }
+                        return null;
+                      }),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          icon: Icon(Iconsax.call),
+                          label: Text("Phone Number"),
+                          hintText: "9553052451"),
+                      controller: phonenumberController,
+                      keyboardType: TextInputType.phone,
+                      validator: ((value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Phone number';
+                        }
+                        if (!value.isPhoneNumber) {
+                          return 'Please enter a valid Phone number';
+                        }
+                        return null;
+                      }),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      maxLines: 3,
+                      decoration: const InputDecoration(
+                          icon: Icon(Iconsax.location),
+                          label: Text("Address"),
+                          hintText: "Where does your customer live"),
+                      controller: addressController,
+                    ),
+                  ),
+                  PrimaryButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (formkey.currentState!.validate()) {
+                        // ... Navigate To next page
+                        _customerDetailsController.name.value =
+                            nameController.text;
+                        _customerDetailsController.email.value =
+                            emailController.text;
+                        _customerDetailsController.phone.value =
+                            phonenumberController.text;
+                        _customerDetailsController.address.value =
+                            addressController.text;
+                        tabController.animateTo(2);
+                      }
+                    },
+                    buttonTitle: "Continue",
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
