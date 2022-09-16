5b47a9ad66e17124a50db94ec21a6dbae18d3415
manas1820 <manasgupta1820@gmail.com>
2022-09-17 03:19:20 +0530

    refactor : cleaning unwanted code

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -1,7 +1,48 @@
 import 'package:get/get.dart';
+import 'package:migo/models/product/product.dart';
+import 'package:migo/service/product_service.dart';
 
 class ProductController extends GetxController {
+  late final ProductServices _productService;
   RxInt quantity = 0.obs;
+  var isLoading = true.obs;
+  var productList = <Product>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _productService = Get.put(ProductServices());
+  }
+
+  void fetchProducts(String category, String query) async {
+    try {
+      isLoading(true);
+      var products = await _productService
+          .fetchProducts(Keyword(category: category, query: query));
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void fetchAllProducts() async {
+    try {
+      isLoading(true);
+      print("done fetching all products");
+      var products = await _productService.fetchAllProducts();
+      print(products.toString());
+      print("done fetching all products 1");
+      if (products != null) {
+        print("done fetching all products 2");
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+
   addProduct() {
     quantity.value = quantity.value++;
   }
