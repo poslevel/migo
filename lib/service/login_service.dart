4e4969f6746ee2126ab2c2f150aa22b100a62eec
manas1820 <manasgupta1820@gmail.com>
2022-09-16 13:00:39 +0530

    feat : login flow and state management

---
+++ lib/service/login_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:get/get_connect/connect.dart' hide Response;
+import 'package:get/get_connect/http/src/status/http_status.dart';
+import 'package:dio/dio.dart';
+import '../models/authentication/login_request_model.dart';
+import '../models/authentication/login_response_model.dart';
+
+/// LoginService responsible to communicate with web-server
+/// via authenticaton related APIs
+class LoginService extends GetConnect {
+  final String loginUrl =
+      'https://backpos.herokuapp.com/api/v1/accounts/login/';
+  Future<LoginResponseModel?> fetchLogin(LoginRequestModel model) async {
+    var dio = Dio();
+    Response response =
+        await dio.post(loginUrl, data: json.encode(model.toJson()));
+    print(response.data.toString());
+    if (response.statusCode == HttpStatus.ok) {
+      return LoginResponseModel.fromJson(response.data);
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/view/auth/login_view_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:migo/models/authManager.dart';
+import 'package:migo/models/authentication/login_request_model.dart';
+import 'package:migo/service/login_service.dart';
+
+class LoginViewModel extends GetxController {
+  late final LoginService _loginService;
+  late final AuthenticationManager _authManager;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loginService = Get.put(LoginService());
+    _authManager = Get.find();
+  }
+
+  Future<void> loginUser(String email, String password) async {
+    final response = await _loginService
+        .fetchLogin(LoginRequestModel(email: email, password: password));
+
+    if (response != null) {
+      /// Set isLogin to true
+      _authManager.login(response.token);
+      print(response.token);
+    } else {
+      /// Show user a dialog about the error response
+      Get.defaultDialog(
+          middleText: 'User not found!',
+          textConfirm: 'OK',
+          confirmTextColor: Colors.white,
+          onConfirm: () {
+            Get.back();
+          });
+    }
+  }
+}
