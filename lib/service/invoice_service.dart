c36d60b1aae191add2e698b2fd68ab1878b5107e
manas1820 <manasgupta1820@gmail.com>
2022-09-18 16:19:57 +0530

    feat : add  ```api``` connection

---
+++ lib/service/invoice_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:get/get_connect/connect.dart' hide Response;
+import 'package:get/get_connect/http/src/status/http_status.dart';
+import 'package:dio/dio.dart';
+import 'package:migo/models/cache.dart';
+import 'package:migo/models/invoice/invoice.dart';
+
+/// LoginService responsible to communicate with web-server
+/// via authenticaton related APIs
+class InvoiceService extends GetConnect with CacheManager {
+  final String invoiceCreation =
+      'https://backpos.herokuapp.com/api/v1/invoice/';
+  Future<Invoice?> addInvoice(InvoiceCreation model) async {
+    var dio = Dio();
+    Response response =
+        await dio.post(invoiceCreation, data: json.encode(model.toJson()));
+    if (response.statusCode == HttpStatus.ok) {
+      return Invoice.fromJson(response.data);
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<Invoice>?> getAllInvoice() async {
+    var dio = Dio();
+    final token = getToken();
+    dio.options.headers["authorization"] = "Bearer $token";
+    Response response = await dio.get(invoiceCreation);
+    if (response.statusCode == HttpStatus.ok) {
+      InvoiceApiHandler handler = InvoiceApiHandler.fromJson(response.data);
+      List<Invoice>? invoices = handler.getInvoice();
+      return invoices;
+    } else {
+      return null;
+    }
+  }
+}
