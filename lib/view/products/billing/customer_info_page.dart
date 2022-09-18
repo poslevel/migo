0a0544c537330d2ae958987eea6905e16f27d842
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-09-18 12:52:02 +0530

    added get it delivered checkbox in billing

--- lib/view/products/billing/customer_info_page.dart
+++ lib/view/products/billing/customer_info_page.dart
@@ -6,7 +6,7 @@ import 'package:migo/view/responsive.dart';
 import 'package:migo/widgets/billing_page_divider.dart';
 import 'package:migo/widgets/buttons.dart';
 
-class CustomerInfoPage extends StatelessWidget {
+class CustomerInfoPage extends StatefulWidget {
   const CustomerInfoPage({
     Key? key,
     required this.nameController,
@@ -27,102 +27,133 @@ class CustomerInfoPage extends StatelessWidget {
   final InvoiceController invoiceController;
 
   @override
+  State<CustomerInfoPage> createState() => _CustomerInfoPageState();
+}
+
+class _CustomerInfoPageState extends State<CustomerInfoPage> {
+  var delivery = false;
+  @override
   Widget build(BuildContext context) {
     return Form(
-      key: formkey,
+      key: widget.formkey,
       child: Column(
         children: [
           const BillingPageDivider(),
           Expanded(
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.user_octagon),
-                          label: Text("Customer Name"),
-                          hintText: "Jhon Doe"),
-                      controller: nameController,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Name';
-                        }
-                        return null;
-                      }),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Iconsax.sms),
-                        label: Text("E-mail"),
-                        hintText: "jhon.doe@example.com",
+              child: SizedBox(
+                width: Responsive.isDesktop(context)
+                    ? MediaQuery.of(context).size.width / 2
+                    : null,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            icon: Icon(Iconsax.user_octagon),
+                            label: Text("Customer Name"),
+                            hintText: "Jhon Doe"),
+                        controller: widget.nameController,
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Name';
+                          }
+                          return null;
+                        }),
                       ),
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Email';
-                        }
-                        if (!value.isEmail) {
-                          return 'Please enter valid Email';
-                        }
-                        return null;
-                      }),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.call),
-                          label: Text("Phone Number"),
-                          hintText: "9553052451"),
-                      controller: phonenumberController,
-                      keyboardType: TextInputType.phone,
-                      validator: ((value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Phone number';
-                        }
-                        if (!value.isPhoneNumber) {
-                          return 'Please enter a valid Phone number';
-                        }
-                        return null;
-                      }),
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          icon: Icon(Iconsax.sms),
+                          label: Text("E-mail"),
+                          hintText: "jhon.doe@example.com",
+                        ),
+                        controller: widget.emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Email';
+                          }
+                          if (!value.isEmail) {
+                            return 'Please enter valid Email';
+                          }
+                          return null;
+                        }),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: TextFormField(
-                      maxLines: 3,
-                      decoration: const InputDecoration(
-                          icon: Icon(Iconsax.location),
-                          label: Text("Address"),
-                          hintText: "Where does your customer live"),
-                      controller: addressController,
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            icon: Icon(Iconsax.call),
+                            label: Text("Phone Number"),
+                            hintText: "9553052451"),
+                        controller: widget.phonenumberController,
+                        keyboardType: TextInputType.phone,
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Phone number';
+                          }
+                          if (!value.isPhoneNumber) {
+                            return 'Please enter a valid Phone number';
+                          }
+                          return null;
+                        }),
+                      ),
                     ),
-                  ),
-                  PrimaryButton(
-                    onPressed: () {
-                      // Validate returns true if the form is valid, or false otherwise.
-                      if (formkey.currentState!.validate()) {
-                        // ... Navigate To next page
-                        invoiceController.customerName = nameController.text;
-                        invoiceController.customerEmail = emailController.text;
-                        invoiceController.customerPhone =
-                            phonenumberController.text;
-                        invoiceController.customerAddress =
-                            addressController.text;
-                        tabController
-                            .animateTo(Responsive.isMobile(context) ? 3 : 2);
-                      }
-                    },
-                    buttonTitle: "Continue",
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: TextFormField(
+                        maxLines: 3,
+                        decoration: const InputDecoration(
+                            icon: Icon(Iconsax.location),
+                            label: Text("Address"),
+                            hintText: "Where does your customer live"),
+                        controller: widget.addressController,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          shape: const CircleBorder(),
+                          activeColor: Theme.of(context)
+                              .buttonTheme
+                              .colorScheme!
+                              .primary,
+                          value: delivery,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              delivery = value!;
+                            });
+                          },
+                        ),
+                        const Text("Get it delivered to the customer")
+                      ],
+                    ),
+                    PrimaryButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (widget.formkey.currentState!.validate()) {
+                          // ... Navigate To next page
+                          widget.invoiceController.customerName =
+                              widget.nameController.text;
+                          widget.invoiceController.customerEmail =
+                              widget.emailController.text;
+                          widget.invoiceController.customerPhone =
+                              widget.phonenumberController.text;
+                          widget.invoiceController.customerAddress =
+                              widget.addressController.text;
+                          widget.tabController
+                              .animateTo(Responsive.isMobile(context) ? 3 : 2);
+                        }
+                      },
+                      buttonTitle: "Continue",
+                    )
+                  ],
+                ),
               ),
             ),
           ),
